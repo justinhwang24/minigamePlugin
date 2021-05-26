552244fa5389a70cb84c26125add3e06bda780d9
artavasdes <56280139+artavasdes@users.noreply.github.com>
2021-05-26 09:20:59 -0400

    Added server start and end commands

--- src/io/github/minigameplugin/main/Main.java
+++ src/io/github/minigameplugin/main/Main.java
@@ -3,5 +3,18 @@ package io.github.minigameplugin.main;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+	
+	@Override
+	public void onEnable() {
+		//startup
+		//reloads
+		//plugin reloads
+	}
+	
+	@Override
+	public void onDisable() {
+		//shutdown
+		//reloads
+		//plugin reloads
+	}
 }
