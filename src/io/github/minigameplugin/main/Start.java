3fe7222c2c5df4968f66d7aa99a466e6c03e1d0d
artavasdes <56280139+artavasdes@users.noreply.github.com>
2021-06-08 08:28:20 -0400

    Added start method(not complete), tp on join

---
+++ src/io/github/minigameplugin/main/Start.java
@@ -0,0 +1,12 @@
+package io.github.minigameplugin.main;
+
+import java.util.List;
+
+public class Start {
+    public static List<Location> Locations = Arrays.asList(new Location[] {
+            new Location(minigamePlugin.world, 25.0D, 62.0D, 255.0D),
+            new Location(minigamePlugin.world, 26.0D, 62.0D, 254.0D),
+            new Location(minigamePlugin.world, 25.0D, 62.0D, 253.0D),
+            new Location(minigamePlugin.world, 24.0D, 62.0D, 254.0D) });
+  {
+}
