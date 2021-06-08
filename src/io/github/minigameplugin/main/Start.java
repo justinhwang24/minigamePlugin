6c08b81e4c14c737a7bf39b9c0ca6ccb89fedbc4
artavasdes <56280139+artavasdes@users.noreply.github.com>
2021-06-08 09:18:41 -0400

    Major progress on teleport.

--- src/io/github/minigameplugin/main/Start.java
+++ src/io/github/minigameplugin/main/Start.java
@@ -1,10 +1,25 @@
 package io.github.minigameplugin.main;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 public class Start {
 	
 	public static void teleport() {
-		//tp players to set of locations in arena
+		//Teleports players to set locations
+		ArrayList<Location> locations = new ArrayList<Location>();
+		locations.add(world,10,70,10);
+		locations.add(world,13,70,10);
+		locations.add(world,15,70,10);
+		locations.add(world,12,70,10);
+		Collection<Player> players = Bukkit.getOnlinePlayers();
+		for(Player : players) {
+			players.get(i).teleport(locations.get(i));
+		}
 		
 	}
 	
