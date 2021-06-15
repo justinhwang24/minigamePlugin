8d53a8c4baebdc451d9fefe9b7994bdb68308006
Justin Hwang <advancedjus@gmail.com>
2021-06-15 18:03:04 -0400

    fixed tnt speed

--- src/io/github/minigameplugin/main/Start.java
+++ src/io/github/minigameplugin/main/Start.java
@@ -77,7 +77,7 @@ public class Start {
 				Location loc = new Location(world, randomX, y, randomZ);
 				world.spawnEntity(loc, EntityType.PRIMED_TNT);
 			}
-		}.runTaskTimer(Main.instance, 0L, 3L);
+		}.runTaskTimer(Main.instance, 0L, 2L);
 	}
 	
 	public static void checkForWin() throws InterruptedException {
