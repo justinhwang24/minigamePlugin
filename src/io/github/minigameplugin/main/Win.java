97d1a2de783199ccaf6a6eaf3bfbab325adb5b0f
Vincent Kim <vinceentk@gmail.com>
2021-06-16 08:46:19 -0400

    Changed Win message

--- src/io/github/minigameplugin/main/Win.java
+++ src/io/github/minigameplugin/main/Win.java
@@ -11,7 +11,7 @@ public class Win {
     	if (Start.alive.size() == 1) {
 	    	//Prints name of last player alive
     		for (Player p : Bukkit.getOnlinePlayers()) {
-    			p.sendMessage(ChatColor.AQUA + Start.alive.get(0).getName() + " won the game!");
+    			p.sendMessage(ChatColor.AQUA + Start.alive.get(0).getName() + " has won the game!");
     			p.setGameMode(GameMode.ADVENTURE);
 		    	Reset.original(p);
     		}
